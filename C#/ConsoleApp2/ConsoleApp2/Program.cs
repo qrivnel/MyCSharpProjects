a552f580893d9d8b2e48d319d5196905d75e3578
qrivnel <120566447+qrivnel@users.noreply.github.com>
2023-09-22 22:24:27 +0300

    Add files via upload

---
+++ C#/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+public class Personel
+{
+    public string AdSoyad;
+    public int SicilNo;
+    public double Maas;
+
+    public Personel(string adSoyad, int sicilNo, double maas)
+    {
+        AdSoyad = adSoyad;
+        SicilNo = sicilNo;
+        Maas = maas;
+    }
+}
+
+public class Node
+{
+    public Personel data;
+    public Node next;
+
+    public Node(Personel p)
+    {
+        data = p;
+        next = null;
+    }
+}
+
+public class LinkedList
+{
+    public Node head;
+
+    public void PrintList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+            Console.WriteLine(temp.data.AdSoyad + " - " + temp.data.SicilNo + " - " + temp.data.Maas);
+            temp = temp.next;
+        }
+    }
+
+    public void Append(Personel p)
+    {
+        Node newNode = new Node(p);
+        if (head == null)
+        {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null)
+        {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    public Node SimpleSearch(int sicilNo)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+            if (temp.data.SicilNo == sicilNo)
+            {
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    public Node SortedSearch(int sicilNo)
+    {
+        Node temp = head;
+        while (temp != null && temp.data.SicilNo < sicilNo)
+        {
+            temp = temp.next;
+        }
+        if (temp != null && temp.data.SicilNo == sicilNo)
+        {
+            return temp;
+        }
+        return null;
+    }
+
+    public void Insert(Personel p)
+    {
+        Node newNode = new Node(p);
+        if (head == null)
+        {
+            head = newNode;
+            return;
+        }
+        if (p.SicilNo < head.data.SicilNo)
+        {
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null && temp.next.data.SicilNo < p.SicilNo)
+        {
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public void SortedInsert(Personel p)
+    {
+        Node newNode = new Node(p);
+        if (head == null || p.SicilNo < head.data.SicilNo)
+        {
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null && temp.next.data.SicilNo < p.SicilNo)
+        {
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+}
---
+++ C#/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+class Personel
+{
+    public string ad;
+    public string soyad;
+    public int sicilNo;
+    public double maas;
+
+    public Personel(string ad, string soyad, int sicilNo, double maas)
+    {
+        this.ad = ad;
+        this.soyad = soyad;
+        this.sicilNo = sicilNo;
+        this.maas = maas;
+    }
+}
+
+class Node
+{
+    public Personel veri;
+    public Node sonraki;
+
+    public Node(Personel veri)
+    {
+        this.veri = veri;
+        this.sonraki = null;
+    }
+}
+
+class BagliListe
+{
+    private Node baslangic;
+
+    public BagliListe()
+    {
+        this.baslangic = null;
+    }
+
+    public void ElemanEkle(Personel veri)
+    {
+        Node yeni = new Node(veri);
+
+        if (baslangic == null)
+        {
+            baslangic = yeni;
+        }
+        else
+        {
+            Node sonraki = baslangic;
+            while (sonraki.sonraki != null)
+            {
+                sonraki = sonraki.sonraki;
+            }
+            sonraki.sonraki = yeni;
+        }
+    }
+
+    public void ElemanYazdir()
+    {
+        Node gecici = baslangic;
+        while (gecici != null)
+        {
+            Console.WriteLine(gecici.veri.ad + " " + gecici.veri.soyad + " " + gecici.veri.sicilNo + " " + gecici.veri.maas);
+            gecici = gecici.sonraki;
+        }
+    }
+
+    public Node ElemanAra(int sicilNo)
+    {
+        Node gecici = baslangic;
+        while (gecici != null)
+        {
+            if (gecici.veri.sicilNo == sicilNo)
+            {
+                return gecici;
+            }
+            gecici = gecici.sonraki;
+        }
+        return null;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BagliListe liste = new BagliListe();
+
+        Personel personel1 = new Personel("Ali", "Yılmaz", 1001, 3000.0);
+        Personel personel2 = new Personel("Ayşe", "Demir", 1002, 3500.0);
+        Personel personel3 = new Personel("Mehmet", "Kara", 1003, 4000.0);
+
+        liste.ElemanEkle(personel1);
+        liste.ElemanEkle(personel2);
+        liste.ElemanEkle(personel3);
+
+        Console.WriteLine("Personeller:");
+        liste.ElemanYazdir();
+
+        Console.WriteLine();
+
+        int arananSicilNo = 1002;
+        Node bulunanNode = liste.ElemanAra(arananSicilNo);
+        if (bulunanNode != null)
+        {
+            Console.WriteLine(arananSicilNo + " sicil numaralı personel bulundu:");
+            Console.WriteLine(bulunanNode.veri.ad + " " + bulunanNode.veri.soyad + " " + bulunanNode.veri.sicilNo + " " + bulunanNode.veri.maas);
+        }
+        else
+        {
+            Console.WriteLine(arananSicilNo + " sicil numaralı personel bulunamadı.");
+        }
+        Console.ReadKey();
+    }
+}
